8e675db47ed53e2e155677a1094e132a281fd5fb
nokatz <nokacpetrusicki@yahoo.com>
2020-01-27 18:38:06 +0100

    basic functionality, listening keyup events

---
+++ main.js
@@ -0,0 +1,37 @@
+﻿
+
+let text, typed; 
+let textString, typedString = "", substring;
+
+text = document.getElementById('text');
+textString = text.textContent;
+
+typed = document.getElementById('typed');
+typed.contentEditable = 'true';
+typed.focus();
+
+
+typed.addEventListener('keyup', typeKey);
+
+function typeKey(e){
+	
+	typedString = typed.textContent;
+	
+	subString = textString.slice(0, typedString.length);
+
+	checkText();
+	
+}
+
+function checkText(){
+	
+	if(typedString !== subString){
+		typed.style.color = "red";
+	} else {
+		typed.style.color = "black";
+		if(typedString.length === textString.length){
+			alert("Correct!");
+		}
+	}	
+	
+}
